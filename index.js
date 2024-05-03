12f59ab1d4f86f7736ca48bef83696c6aaf8f568
Tommi <83519567+MrRodent@users.noreply.github.com>
2024-05-03 13:46:17 +0300

    part3-5

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
 let notes = [  
   {    
     id: 1,    
@@ -39,18 +49,27 @@ app.get('/', (request, response) => {
   response.send('<h1>Hello world</h1>')
 })
 
+// Hae kaikki
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes)
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
-  Note.findById(request.params.id).then(note => {
-    response.json(note)
-  })
+// Hae yksittäinen
+app.get('/api/notes/:id', (request, response, next) => {
+  Note.findById(request.params.id)
+    .then(note => {
+      if (note) {
+        response.json(note)
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
 })
 
+// Luo uusi
 app.post('/api/notes', (request, response) => {
   const body = request.body
 
@@ -68,14 +87,35 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
+// Poisto
 app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
-
-  response.status(204).end()
+  Note.findByIdAndDelete(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
+// Päivitys (make important)
+// Mahdollistaa myös sisällön editoinnin
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+
+  const note = {
+    content: body.content,
+    important: body.important,
+  }
+
+  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+    .then(updatedNote => {
+      response.json(updatedNote)
+    })
+    .catch(error => next(error))
+})
+
+
 app.use(unknownEndpoint)
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT)
