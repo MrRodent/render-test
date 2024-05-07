9438db0c680499dd663d6a8cff720ad2ea48e783
Tommi <83519567+MrRodent@users.noreply.github.com>
2024-05-07 16:48:07 +0300

    uibuild

--- index.js
+++ index.js
@@ -18,6 +18,9 @@ const errorHandler = (error, request, response, next) => {
   if (error.name === 'CastError') {
     return response.status(400).send({ error: 'malformatted id' })
   }
+  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
@@ -70,21 +73,19 @@ app.get('/api/notes/:id', (request, response, next) => {
 })
 
 // Luo uusi
-app.post('/api/notes', (request, response) => {
+app.post('/api/notes', (request, response, next) => {
   const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
   })
 
-  note.save().then(savedNote => {
+  note.save()
+    .then(savedNote => {
     response.json(savedNote)
   })
+    .catch(error => next(error))
 })
 
 // Poisto
@@ -99,14 +100,13 @@ app.delete('/api/notes/:id', (request, response) => {
 // Päivitys (make important)
 // Mahdollistaa myös sisällön editoinnin
 app.put('/api/notes/:id', (request, response, next) => {
-  const body = request.body
+  const { content, important } = request.body
 
-  const note = {
-    content: body.content,
-    important: body.important,
-  }
-
-  Note.findByIdAndUpdate(request.params.id, note, { new: true })
+  Note.findByIdAndUpdate(
+    request.params.id, 
+    { content, important }, 
+    { new: true, runValidators: true, context: 'query' }
+  )
     .then(updatedNote => {
       response.json(updatedNote)
     })
