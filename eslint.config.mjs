6427b67d860ed07f1b77a03e8af36ccc2da620b2
Tommi <83519567+MrRodent@users.noreply.github.com>
2024-05-08 18:38:19 +0300

    lint config fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,13 +4,14 @@ import pluginJs from "@eslint/js";
 
 export default [
   {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.browser }},
+  {languageOptions: { globals: globals.node }},
   pluginJs.configs.recommended,
   {
     ignores: ["dist", "eslint.config.mjs"]
   },
   {
     rules: {
+      'no-unused-vars': 'warn',
       'eqeqeq': 'error',
       'no-trailing-spaces': 'error',
       'object-curly-spacing': [
